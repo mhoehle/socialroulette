bb3ad319bad552d99e3e432fe9383620f4ad12c4
hoehleatsu <>
2021-04-27 01:17:44 +0200

    more examples

--- R/socialroulette.R
+++ R/socialroulette.R
@@ -1,19 +1,40 @@
-#' @import tidyverse
-NULL
-
 #' Maximally diverse grouping problem solver based on Lai and Hao (2016)
 #'
-#' The following
+#' Given a distance metric for each possible pair in the frame of n individuals,
+#' find a partition into as many groups as possible with group size at least m. This
+#' is an instance of the maximallz diverse grouping problem, which we use the algorithm
+#' by Lai and Hao (2016) to solve.
+#'
+#' The maximally diverse grouping problem is about partitioning \eqn{n} individuals
+#' into groups of size at least \eqn{m} while maximizing a sum of utility values computed
+#' by summing the utility \eqn{u(i,j)} over all individuals \eqn{i},\eqn{j} partitioned into the same group.
+#' and summing this quantity over all groups. More formally, let \eqn{d_{ij}} denote the number of
+#' time unit (typically days) ago, that individual \eqn{i} and \eqn{j} were in the same group.
+#' Note: This distance is derived by looking at the previous partitions and it is a matter
+#' of definition what this value should be, if \eqn{i} and \eqn{j} have not previously been
+#' in the same group. Let \eqn{n_g=n\> \text{div}\> m} denote the resulting number of groups where \eqn{\text{div}} denotes integer division.
+#' For a given partition let \eqn{x_{ig}} be an indicator variable, which is 1, if \eqn{i} is assigned into
+#' group \eqn{g} and zero otherwise.
+#' A solver of the maximally diverse grouping problem now tries to maximize
+#' \deqn{\sum_{g=1}^G \sum_{i=1}^n \sum_{j=i+1}^n d_{ij} x_{ig} x_{jg},}
+#' subject to the conditions
+#' \deqn{\sum_{g=1}^{G} x_{ig} = 1, \quad i=1,\ldots,n,}
+#' \deqn{\sum_{i=1}^n x_{ig} = n_g, \quad g=1,\ldots,G,}
+#' \deqn{x_{ig} \in \{0,1\}, \quad i=1,\ldots,n; g=1,\ldots,G,}
+#' where \eqn{n_g} is the size of group \eqn{g}, i.e. \eqn{\sum_{g=1}^G n_g = n}. We shall adopt the convention
+#' that group sizes are determined by assigning the labels \eqn{1,\ldots,G} to all individuals and then count
+#' how often each label occurs. This means, e.g., that for \eqn{n=7} and \eqn{m=4} we get \eqn{n_g=7\> \text{div}\> 4=1} group with 7 members.
+#'
+#' Note: The code calls C++ code by Xiangjing Lai and Jin-Kao Hao,
+#' for this several temporary files are generated using `tempfile()`.
+#'
 #' @param mdg_format_file Path to the file containing the MDGP specification in mdgplib format
 #' @param time_limit Number of seconds to iteratively optimize each run
 #' @return File name of the solution file
 #'
-#' @description The code calls C++ code by Xiangjing Lai and Jin-Kao Hao,
-#' for this several temporary files are generated using `tempfile()`.
-#' @details The maximally diverse grouping problem is about partitioning $n$ individuals
-#' into groups of size at least $m$ while maximizing a sum of utility values computed
-#' by summing the utility $u(i,j)$ over all individuals $i,j$ partitioned into the same group.
-#' and summing this quantity over all groups.
+#' @references Xiangjing Lai and Jin-Kao Hao (2016). *Iterated maxima search for the maximally
+#' diverse grouping problem*. European Journal of Operational Research, 254(3), pp. 780-800,
+#' https://doi.org/10.1016/j.ejor.2016.05.018
 #'
 #' @author Xiangjing Lai and Jin-Kao Hao, R interface by M. Höhle
 #' @useDynLib socialroulette mdgp
@@ -37,21 +58,24 @@ mdgp_solver <- function(mdgp_format_file, time_limit= 30) {
   return(list(output_file=output_file, solution_file=solution_file))
 }
 
-#' Convert session specification to Lai and Hao (2016) MDGP solver input specification
+#' Convert partition specification to Lai and Hao (2016) MDGP solver input specification.
+#'
+#' Generate a \code{tempfile()} containing the appropriate specification formed from \code{current_frame} and \code{past_sessions}.
+#'
+#' The specification format has the following header line
+#' \deqn{n\> m\> ds\> n_1\> n_1\> \ldots n_G \> n_G}
+#' Here, each group size \eqn{n_g, g=1,\ldots,G} is repeated twice, because the solver has the opportunity to specify a lower as well as an
+#' upper boundary for each group size. The keyword `ds` for the solver in the above means that the groups can be of different sizes as determined by the respective \eqn{n_g}.
 #'
 #' @param current_frame A pair-distance tibble. The frame needs to contain a column denoted `id`
 #' @param past_sessions A list of sessions. Each sessions consists of a session specification in list format.
 #' @param m The minimum group size, i.e. all groups have size at least m
 #' @return File name of the generated specification file
 #' @examples
-#'  ppl <- tibble(id=sprintf("id%.3d", 1:100))
-#'  session_dates <- seq(as.Date("2021-04-01"), length.out=4, by="1 week")
-#'  frames <- map_df( session_dates, ~ ppl %>% slice_sample(n = rbinom(1,nrow(ppl), prob=0.7)) %>% mutate(date=.x))
-#'  past_sessions <- frames %>% group_split(date) %>% map(~sample_groups(.x, m=4)) %>% setNames(session_dates) %>% .[1:3]
-#'  current_frame <- frames %>% filter(date == max(date))
-#'  spec_file <- write_mdgp_specfile(current_frame, past_sessions, m=4)
-#'  mdgp_solver(spec_file)
-
+#' frame  <- tibble::tibble(id=sprintf("id%.3d", 1:5), date=as.Date("2021-04-27"))
+#' round1 <- list(c("id02", "id03", "id04"), c("id05", "id01"))
+#' spec_file <- socialroulette:::write_mdgp_specfile(frame, past_sessions=list("2021-04-21"=round1), m=2)
+#' cat(stringr::str_c(readLines(spec_file), collapse="\n"))
 write_mdgp_specfile <- function(current_frame, past_sessions, m) {
   # Check that the current_frame has an id column
   stopifnot( "id" %in% colnames(current_frame))
@@ -67,27 +91,27 @@ write_mdgp_specfile <- function(current_frame, past_sessions, m) {
   # Vector containing min and max of each group
   g_spec <- sort(rep(as.numeric(names(n_groupsize)), times=n_groupsize), decreasing=TRUE)
   g_spec <- rep(g_spec, each=2)
-  spec <- str_c(n, " ",n_g, " ds ", str_c(g_spec, collapse=" "))
+  spec <- stringr::str_c(n, " ",n_g, " ds ", stringr::str_c(g_spec, collapse=" "))
 
   # Compute distances to last meet for each id in current_frame
   dist_frame <- sessions_to_distance(current_frame, past_sessions)
 
   # Make alternative index number (starting from zero) for MDGP-solver instead of the id
-  ids <- current_frame %>% mutate(idx = row_number() - 1) %>% select(id, idx)
+  ids <- current_frame %>% dplyr::mutate(idx = row_number() - 1) %>% dplyr::select(id, idx)
   # and add this to the dist_frame
-  dist_frame <-  left_join(dist_frame, ids, by=c("id1"="id")) %>%
-    left_join(ids, by=c("id2"="id")) %>%
-    rename(idx.id1 = idx.x, idx.id2=idx.y) %>%
+  dist_frame <-  dplyr::left_join(dist_frame, ids, by=c("id1"="id")) %>%
+    dplyr::left_join(ids, by=c("id2"="id")) %>%
+    dplyr::rename(idx.id1 = idx.x, idx.id2=idx.y) %>%
     #Make sure idx.id1 < idx.id2 as this is needed for the solver
-    mutate( idx.id1.ord = pmin(idx.id1, idx.id2),
+    dplyr::mutate( idx.id1.ord = pmin(idx.id1, idx.id2),
             idx.id2.ord = pmax(idx.id1, idx.id2)) %>%
-    arrange(idx.id1.ord, idx.id2.ord)
+    dplyr::arrange(idx.id1.ord, idx.id2.ord)
 
   #Sanity check
   stopifnot( all(dist_frame$idx.id1.ord < dist_frame$idx.id2.ord))
 
   #Make a temporary file
-  tmp_file <- str_c(tempfile(), ".txt")
+  tmp_file <- stringr::str_c(tempfile(), ".txt")
   writeLines(spec, tmp_file)
   write_delim(dist_frame %>% select(idx.id1.ord,idx.id2.ord,dist), path=tmp_file, col_names=FALSE, append=TRUE)
 
@@ -111,9 +135,9 @@ read_mdgp_solutionfile <- function(file_name) {
 
   #Read the groups
   groups <- read_delim(file=file_name, skip=1 + n_g, delim=" ", col_names=c("id_int", "group")) %>%
-    mutate(idx = as.numeric(id_int)+1,
+    dplyr::mutate(idx = as.numeric(id_int)+1,
            group = as.numeric(group) + 1) %>%
-    select(idx, group)
+    dplyr::select(idx, group)
 
   return(groups)
 }
@@ -122,8 +146,9 @@ read_mdgp_solutionfile <- function(file_name) {
 #'
 #' @param frame tibble containing a column `id` of the sampling units (e.g. people)
 #' @param m requested group size (integer). If m not a divisor of nrow(people) then put leftovers in existing groups
-#'
 #' @return A list of vectors where each vector containing the ids (row number in frame) of those belonging to the same group
+#'
+#' @keywords internal
 
 make_partition_srs <- function(frame, m) {
   # Number of groups needed
@@ -142,6 +167,27 @@ make_partition_srs <- function(frame, m) {
   return(groups)
 }
 
+#' Make all pairs within a group
+#'
+#' @param group A vector of ids belonging to the same group
+#' @return a data.frame containing all pairs with id of the first being smaller than the id of the second entry
+#' @keywords internal
+group_to_pairs <- function(group) {
+  tidyr::expand_grid(id1=group, id2=group) %>% filter(id1 < id2)
+}
+
+#' Convert a list of partitionLists into a data.frame with all pairs
+#'
+#' The name of the list contain the date at which each partition was used.
+#' @param sessions A list of past partitions, i.e. a list where each entry is a partitionList
+#' @examples
+#' sessions <- list("2021-04-21"=list(c("id02", "id03", "id04"), c("id05", "id01")),
+#'                   "2021-04-27"=list(c("id05", "id03", "id04"), c("id02", "id01")))
+#' socialroulette:::sessions_to_pairs(sessions)
+
+sessions_to_pairs <- function(sessions) {
+  map_dfr(sessions,  ~ map_df(.x, ~ group_to_pairs(.x)), .id="date")
+}
 
 #' Convert a list of sessions into a MDGP distance metric
 #'
@@ -150,23 +196,17 @@ make_partition_srs <- function(frame, m) {
 #'
 #' @description A session is a data.frame containing the columns id1, id2 and date
 #' @examples
-#'  ppl <- data.frame(id=sprintf("id%.3d", 1:100))
-#'  session_dates <- seq(as.Date("2021-04-01"), length.out=4, by="1 week")
-#'  frames <- map_df( session_dates, ~ ppl %>% slice_sample(n = rbinom(1,nrow(ppl), prob=0.7)) %>% mutate(date=.x))
-#'  past_sessions <- frames %>% group_split(date) %>% map(~ rsocialroulette(.x, past_sessions=NULL, m=4, algorithm="srs")) %>% setNames(session_dates) %>% .[1:3]
-#'  current_frame <- frames %>% filter(date == max(date))
-#'  sessions_to_distance( current_frame, past_sessions) %>% pull(dist) %>% table()
-sessions_to_distance <- function(current_frame, past_sessions) {
-  # Make all pairs within a group
-  group_to_pairs <- function(group) {
-    expand_grid(id1=group, id2=group) %>% filter(id1 < id2)
-  }
+#' frame  <- tibble::tibble(id=sprintf("id%.2d", 1:5), date=as.Date("2021-04-27"))
+#' sessions <- list("2021-04-14"=list(c("id02", "id03", "id04"), c("id05", "id01")),
+#'                   "2021-04-21"=list(c("id05", "id03", "id04"), c("id02", "id01")))
+#' socialroulette:::sessions_to_distance(frame, sessions)
 
+sessions_to_distance <- function(current_frame, past_sessions) {
   #Make all potential pairs in current_frame
-  current_pairs <- expand_grid(id1=current_frame %>%  pull(id),
+  current_pairs <- tidyr::expand_grid(id1=current_frame %>%  pull(id),
                                id2=current_frame %>%  pull(id)) %>%
-    filter(id1 < id2) %>%
-    mutate(date = current_frame$date[1])
+    dplyr::filter(id1 < id2) %>%
+    dplyr::mutate(date = current_frame$date[1])
 
   #If there are no past sessions then no need to do more.
   if (is.null(past_sessions)) {
@@ -174,10 +214,15 @@ sessions_to_distance <- function(current_frame, past_sessions) {
   }
 
   #Make the pairs data.frame for each session
-  past_pairs <- map_dfr(past_sessions,  ~ map_df(.x, ~ group_to_pairs(.x)), .id="date")
+  past_pairs <- sessions_to_pairs( past_sessions) #map_dfr(past_sessions,  ~ map_df(.x, ~ group_to_pairs(.x)), .id="date")
 
-  #Today distance
-  Delta <- past_pairs %>% pull(date) %>% unique() %>% as.Date() %>% diff() %>% mean() %>% as.numeric()
+  #Define distance to today for those who have not met so far - special case if only one past value
+  Delta <- past_pairs %>% dplyr::pull(date) %>% unique() %>% as.Date() %>% sort()
+  if (length(Delta)>1) {
+    Delta <- Delta %>% diff() %>% mean() %>% as.numeric()
+  } else {
+    Delta <- 1
+  }
   date_no_meet <- (past_pairs %>% pull(date) %>% as.Date() %>%  min() ) - Delta
   dist_today <- ((current_pairs %>% pull(date) %>% .[[1]]) - date_no_meet) %>% as.numeric()
 
@@ -191,7 +236,9 @@ sessions_to_distance <- function(current_frame, past_sessions) {
   return(current_dist %>% rename(date = `date.current`) %>% select(-date.past))
 }
 
-#' Take a frame including a grouping column and convert this to a partitionList
+#' Take a frame including a `group` column and convert this to a corresponding partitionList
+#'
+#' This is a small useful converter function
 #'
 #' @param frame The frame with `id` and `group` columns to convert
 #' @return A list of vectors, where each vector contains all individuals in the corresponding group
@@ -200,13 +247,17 @@ frame_to_partitionList <- function(frame) {
   map(seq_len(n_g), ~ frame %>% filter(group == .x) %>% pull(id))
 }
 
-#' Take a partitionList and convert it to a frame including a grouping column
+#' Take a partitionList and convert it to frame representation
+#'
+#' The resulting \code{tibble} will have an additional column \code{group}
 #'
 #' @param A partitionList, i.e. list of vectors, where each vector contains all individuals in the corresponding group
 #' @return frame The frame with `id` and `group` columns to convert
-
+#' @examples
+#' round1 <- list(c("id02", "id03", "id04"), c("id05", "id01"))
+#' socialroulette:::partitionList_to_frame(round1)
 partitionList_to_frame <- function(l) {
-  map_df( seq_len(length(l)), ~ tibble(id=l[[.x]], group=.x))
+  purrr::map_df( seq_len(length(l)), ~ tibble::tibble(id=l[[.x]], group=.x))
 }
 
 
@@ -232,29 +283,6 @@ partition_to_frame <- function(partition, frame ) {
 #' @param m minimum group size, i.e. all groups will be at least size m.
 #' @return A partitionList containing the partitioning of current_frame maximizing the overall sum of gossip to be exchanged.
 #' @export
-#' @examples
-#' library(socialroulette)
-#' #Distribute 5 ppl into groups of at least 2
-#' frame <- tibble(id=sprintf("id%.3d@math.su.se", 1:5), date=Sys.Date())
-#' rsocialroulette(frame, past_sessions=NULL, m=2, algorithm="srs")
-#'
-#' #Same, but to test that algorithm="mdgp" also works with past_session=NULL
-#' rsocialroulette(frame, past_sessions=NULL, m=2, algorithm="mdgp")
-#'
-#'# Create a history of previous sessions as well as the current frame from a population of 100
-#' ppl <- tibble(id=sprintf("id%.3d@math.su.se", 1:100))
-#' session_dates <- seq(as.Date("2021-04-01"), length.out=4, by="1 week")
-#' # Simulate changing participation each week (with prob 0.7 they will attend)
-#' frames <- map_df( session_dates, ~ ppl %>% slice_sample(n = rbinom(1,nrow(.), prob=0.7)) %>% mutate(date=.x))
-#' # Frame for the current session is the last one
-#' current_frame <- frames %>% filter(date == max(date))
-#' # Simulate the partitions for the last 3 weeks
-#' past_sessions <- frames %>% filter(date < max(session_dates)) %>%
-#'   group_split(date) %>%
-#'   map(~rsocialroulette(current_frame=.x, past_sessions=NULL, m=4)) %>%
-#'   setNames(session_dates[1:3])
-#' # Make the partition, which tries to bring together those who have not met in a long time
-#' rsocialroulette(current_frame, past_session, m=4, algorithm="mdgp")
 rsocialroulette <- function(current_frame, past_sessions, m, algorithm=c("mdgp", "srs")) {
   # Sanity checks
   stopifnot( "id" %in% colnames(current_frame))
@@ -263,7 +291,7 @@ rsocialroulette <- function(current_frame, past_sessions, m, algorithm=c("mdgp",
   algorithm <- match.arg(algorithm, choices=c("mdgp", "srs"))
 
   #Debug info
-  cat(str_c("Partitioning ", nrow(current_frame), " individuals into groups of at least ", m, ".\n"))
+  cat(stringr::str_c("Partitioning ", nrow(current_frame), " individuals into groups of at least ", m, ".\n"))
 
   #Read output and convert it to a partitionList
   if (algorithm == "mdgp") {
@@ -281,41 +309,7 @@ rsocialroulette <- function(current_frame, past_sessions, m, algorithm=c("mdgp",
   #Group sizes etc.
   groups <- map_dbl(partitionList, length)
 
-  cat(str_c("Created ", length(groups), " groups of sizes ", str_c(groups, collapse=" "), ".\n"))
+  cat(stringr::str_c("Created ", length(groups), " groups of sizes ", stringr::str_c(groups, collapse=" "), ".\n"))
 
   return(partitionList)
 }
-
-testit <- function() {
-   #library(socialroulette)
-   ppl <- tibble(id=sprintf("id%.3d@math.su.se", 1:100))
-   session_dates <- seq(as.Date("2021-04-01"), length.out=4, by="1 week")
-   frames <- map_df( session_dates, ~ ppl %>% slice_sample(n = rbinom(1,nrow(ppl), prob=0.7)) %>% mutate(date=.x))
-   past_sessions <- frames %>% group_split(date) %>% map(~rsocialroulette(.x, past_sessions=NULL, m=4, algorithm="srs")) %>% setNames(session_dates) %>% .[1:3]
-   current_frame <- frames %>% filter(date == max(date))
-
-   spec_file <- write_mdgp_specfile(current_frame, past_sessions, m=4)
-   res <- mdgp_solver(spec_file)
-   current_frame_with_group <- read_mdgp_solutionfile(res$solution_file) %>%
-     partition_to_frame(frame=current_frame)
-
-   # Show group sizes
-   current_frame_with_group %>% group_by(group) %>% summarise(n=n()) %>%
-     pull(n) %>% table()
-
-   #Make a partitionList for the current session
-   current_partitionList <-current_frame_with_group %>% frame_to_partitionList()
-   current_partitionList
-
-   #Extend the past sessions
-   sessions <- past_sessions
-   sessions[[current_frame_with_group %>% slice(1) %>% pull(date) %>% as.character()]] <- current_partitionList
-   sessions
-
-   #Check if anyone has a distance of zero?
-   sessions_to_distance(current_frame, sessions) %>% pull(dist)
-
-   #R CMD INSTALL .
-   #Rscript -e "library(socialroulette) ; mdgp()"
-}
-
--- README.md
+++ README.md
@@ -1,29 +1,17 @@
-# socialroulette
-R package for partitioning individuals into groups
+# socialroulette: A package to generate social groupings
+
+A package for partitioning individuals into groups of a pre-specified size. This can be as simple as using simple random sampling (srs) to divide n individuals into groups of size at least m. If one keeps track of the past partitions then an additional
+aim can be to try to maximize the time that people have a reunion, i.e. end up in the same group. This boils
+down to an instance of the maximally diverse grouping problem.
+See the [package website](https://hoehleatsu.github.io/socialroulette/)  for more information, documentation and examples.
+
 ```
+# Install package
+devtools::install_github("hoehle/socialroulette")
+
 # Distribute 5 ppl into groups of at least 2
 frame <- tibble(id=sprintf("id%.3d@math.su.se", 1:5), date=Sys.Date())
 rsocialroulette(frame, past_sessions=NULL, m=2, algorithm="srs")
-
-
-# Create a history of previous sessions as well as the current frame from a population of 100
-ppl <- tibble(id=sprintf("id%.3d@math.su.se", 1:100))
-session_dates <- seq(as.Date("2021-04-01"), length.out=4, by="1 week")
-
-# Simulate changing participation each week (with prob 0.7 they will attend)
-frames <- map_df( session_dates, ~ ppl %>% slice_sample(n = rbinom(1,nrow(.), prob=0.7)) %>% mutate(date=.x))
-
-# Frame for the current session is the last one
-current_frame <- frames %>% filter(date == max(date))
-
-# Simulate the partitions for the last 3 weeks
-past_sessions <- frames %>% filter(date < max(session_dates)) %>%
-  group_split(date) %>%
-  map(~rsocialroulette(current_frame=.x, past_sessions=NULL, m=4)) %>%
-  setNames(session_dates[1:3])
 ```
 
-## Make the partition, which tries to bring together those who have not met in a long time
-```
-rsocialroulette(current_frame, past_session, m=4, algorithm="mdgp")
-```
+For more information see the `get-started` vignette.
