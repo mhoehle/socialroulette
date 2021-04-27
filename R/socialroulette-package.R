b6ff998446fe9ebf3ff22d068a305b5bb3902641
hoehleatsu <>
2021-04-28 01:18:51 +0200

    more work on documenting the pkg in the vignette. added reverse func pairs_to_partition

---
+++ R/socialroulette-package.R
@@ -0,0 +1,21 @@
+#' socialroulette: A package to generate social groupings
+#'
+#' A package for partitioning individuals into groups of a pre-specified size. This can be as simple as using simple random sampling (srs)
+#' to divide n individuals into groups of size at least m. If one keeps track of the past partitions then an additional
+#' aim can be to try to maximize the time that people have a reunion, i.e. end up in the same group. This boils
+#' down to an instance of the maximally diverse grouping problem.
+#' See the [package website](https://hoehleatsu.github.io/socialroulette/)  for more information, documentation and examples.
+#'
+#' @docType package
+#' @keywords package
+#' @name socialroulette-package
+#'
+#' @import tidyverse
+#' @import dplyr
+#' @import tidyr
+#' @import purrr
+#' @import stringr
+#' @import readr
+#'
+NULL
+
